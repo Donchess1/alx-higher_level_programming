293726d2acd4b5549d26e47af62551a0df8dd6af
Donchess1 <stepabod@yahoo.com>
2023-10-02 20:21:53 +0000

    rec

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,14 +2,14 @@
 
 /**
  * check_cycle - checks if a linked list contains a cycle
- * @lnklst: linked list to check
+ * @list: linked list to check
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int check_cycle(listint_t *lnklst)
+int check_cycle(listint_t *list)
 {
-	listint_t *slow = lnklst;
-	listint_t *fast = lnklst;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!lnklst)
 		return (0);
