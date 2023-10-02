2d435563c99e708f99077d8ee3d769418de76ce8
Donchess1 <stepabod@yahoo.com>
2023-10-02 20:07:50 +0000

    sorted

---
+++ 10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @lnklst: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *lnklst)
+{
+	listint_t *slow = lnklst;
+	listint_t *fast = lnklst;
+
+	if (!lnklst)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
+
