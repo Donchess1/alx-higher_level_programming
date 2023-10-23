839ae097a61137683248b38ca47789687b19df74
Donchess1 <stepabod@yahoo.com>
2023-10-23 13:11:28 +0000

    lh

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x <= 0:
+    if x < 0:
         return (0)
     counter = 0
     for a in range(x):
         try:
-            print(my_list[a], end = " ")
+            print(my_list[a], end = "")
             counter += 1
         except IndexError:
             break
-    print()
+    print("")
     return (counter)
