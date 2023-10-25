cdac1493eecc44f1f564913c1837b7e4337e246b
Donchess1 <stepabod@yahoo.com>
2023-10-25 11:00:06 +0000

    mo

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 "square module"
+
+
 class Square:
     "define the class square"
+
     def __init__(self, size=0):
         "initiate size at 0"
         self.size = size
+
     @property def size(self):
         "set size of square"
         return self.__size
+
     @size.setter def size(self, value):
     if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -18,3 +23,11 @@ class Square:
             "return area"
         result =self.__size * self.__size
         return result
+
+    def my_print(self):
+        "to print the square"
+        for a in range (0, self.__size):
+            [print("#", end = "")
+                    for b in range (0, self.__size)]
+            if self.__size = 0:
+                print("")
