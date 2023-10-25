80f3473e73b659f72090c14e4b39e8c558745caa
Donchess1 <stepabod@yahoo.com>
2023-10-25 14:00:42 +0000

    lint

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,8 +21,5 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-        def area(self):
-            "return area"
         result = (self.__size * self.__size)
         return result
