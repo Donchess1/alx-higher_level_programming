e9d3e73c89c0147c7393c9dbf361a046976d2aa4
Donchess1 <stepabod@yahoo.com>
2023-11-07 20:31:17 +0000

    seven

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,11 +4,10 @@
 
 
 import sys
-
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
 if __name__ == "__main__":
+    from save_to_json_file import ('7-save_to_json_file').save_to_json_file
+    from load_from_json_file import ('8-load_from_json_file').load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
