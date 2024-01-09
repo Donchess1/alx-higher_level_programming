1da66c9a7a08a198e539b9325256d283b582dc80
Donchess1 <stepabod@yahoo.com>
2024-01-09 12:21:13 +0000

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,19 @@
-#!/usr/bin / node
+#!/usr/bin/node
 const SquareP = require('./5-square');
 
 class Square extends SquareP {
-    charPrint (c) {
-        if (c === undefined) {
-            c = "X";
-        }
-        for (let a = 0; a < this.width; a++){
-            let b = c.repeat(this.height);
-            console.log(b);
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
 }
+
 module.exports = Square;
